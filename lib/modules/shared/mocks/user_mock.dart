01186ab38b6e7ddd855cd6ba46cb188f251c05e0
Guilherme Jahnke Vieira <guiguijv@gmail.com>
2023-11-14 23:41:32 -0300

    feat(no-issue): (Remove mocks)

--- lib/modules/shared/mocks/user_mock.dart
+++
@@ -1,9 +0,0 @@
-import '../domain/entities/user.dart';
-
-User get userMock => const User(
-      id: '12345',
-      name: 'João',
-      lastName: 'Silva',
-      email: 'teste@fiap.com',
-      document: '123.456.789-09',
-    );
